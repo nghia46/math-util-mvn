4939b1891cb743ba97ebac72f48776d83ad9e56d
tiptopdragon0406 <=>
2023-06-28 11:34:31 +0700

    add screenShoot & update the factorial funtion

--- src/main/java/com/admin/mathutil/core/MathUtility.java
+++ src/main/java/com/admin/mathutil/core/MathUtility.java
@@ -5,11 +5,22 @@ public class MathUtility {
 
     public static final double PI = 3.1415;
 
+//    public static long getFatorial(int n) {
+//        if (n < 0) {
+//            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+//        } else if (n > 20) {
+//            throw new IllegalArgumentException("Factorial greater than 20 is too big.");
+//        } else if (n == 0 || n == 1) {
+//            return 1;
+//        } else {
+//            return n * getFatorial(n - 1);
+//        }
+//    }
     public static long getFatorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
         } else if (n > 20) {
-            throw new IllegalArgumentException("Factorial greater than 20 is too big.");
+            throw new IllegalArgumentException("Factorial greater than 200 is too big.");
         } else if (n == 0 || n == 1) {
             return 1;
         } else {
